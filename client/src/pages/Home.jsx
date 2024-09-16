f55c92d996f0a21a0ef30fa932f112a0b791ec9f
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-09-16 23:23:08 +0530

    fixed: CSS Changed for landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,12 +8,12 @@ function Home() {
             <main className="flex-1">
                 <HeroSection />
                 <hr />
-                <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+                <section className="w-full py-12 md:py-24 bg-background lg:py-32 xl:py-48">
                     <div className="container px-4 md:px-6">
                         <div className="flex flex-col items-center space-y-4 text-center">
                             <div className="space-y-2">
                                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl text-gray-900 dark:text-gray-100">
-                                    Build Your Dream Website
+                                    This is Hidden Commit
                                 </h1>
                                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                                     With our intuitive website builder, you can
@@ -38,7 +38,7 @@ function Home() {
                         </div>
                     </div>
                 </section>
-                <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
+                <section className="w-full py-12 md:py-24 bg-background lg:py-32">
                     <div className="container px-4 md:px-6">
                         <div className="flex flex-col items-center space-y-4 text-center">
                             <div className="space-y-2">
@@ -55,7 +55,7 @@ function Home() {
                         </div>
                     </div>
                 </section>
-                <section className="w-full py-12 md:py-24 lg:py-32">
+                <section className="w-full py-12 md:py-24 bg-background lg:py-32">
                     <div className="container px-4 md:px-6">
                         <div className="flex flex-col items-center space-y-4 text-center">
                             <div className="space-y-2">
